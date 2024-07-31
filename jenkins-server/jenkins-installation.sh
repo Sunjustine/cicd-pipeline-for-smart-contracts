7407c3178efbcd9ff64b024fd2dddfa5786a2a36
Sunjustine <destrogroups@gmail.com>
2024-07-31 14:48:25 +0300

    Change some settings

--- jenkins-server/jenkins-installation.sh
+++ jenkins-server/jenkins-installation.sh
@@ -3,74 +3,109 @@
 # This script is needed to install Jenkins server
 # on our EC2 instance
 
+# Create or clear the log file
+: > ./output.log
+
+# Function to log output of commands
+log_command() {
+    "$@" >> ./output.log 2>&1
+}
+
 # Update packages
-sudo apt-get update -y;
+log_command sudo apt-get update -y
 
 # Install java openjdk 11
-sudo apt install openjdk-11-jdk -y;
-
+log_command sudo apt install openjdk-11-jdk -y
 
 # Check if Java is installed
-if java --version &> /dev/null
+if java --version &> ./output.log
 then
     # Get the Java version
     java_version=$(java --version 2>&1 | awk -F[\"_] 'NR==1{print $2}')
-    echo "Java version: $java_version"
+    echo "Java version: $java_version" >> ./output.log
 
     # Continue with the rest of the script
     # Add your code here
 
 else
-    echo "Java is not installed. Exiting."
+    echo "ERROR: Java is not installed. Exiting." >> ./output.log
     exit 1
 fi
 
-
 # Let's add the repository key to your system:
-wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key |sudo gpg --dearmor -o /usr/share/keyrings/jenkins.gpg;
+log_command wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo gpg --dearmor -o /usr/share/keyrings/jenkins.gpg
 
 # Next, let’s append the Debian package repository 
 # address to the server’s sources.list:
-sudo sh -c 'echo deb [signed-by=/usr/share/keyrings/jenkins.gpg] http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list';
+log_command sudo sh -c 'echo deb [signed-by=/usr/share/keyrings/jenkins.gpg] http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
 
 # After both commands have been entered, run apt 
 # update so that apt will use the new repository.
-sudo apt update -y;
+log_command sudo apt update -y
 
 # Finally, install Jenkins and its dependencies:
-sudo apt install jenkins -y;
+log_command sudo apt install jenkins -y
 
-# now that Jenkins is installed, start it by using 
+# Now that Jenkins is installed, start it by using 
 # systemctl:
-sudo systemctl start jenkins.service;
+log_command sudo systemctl start jenkins.service
 
-# Enable jenkins service
-sudo systemctl enable jenkins.service;
+# Enable Jenkins service
+log_command sudo systemctl enable jenkins.service
+
+# Run the command to get Jenkins version
+versionOutput=$(jenkins --version 2>&1)
+
+# Check if the command was successful and the output is not empty
+if [[ $? -eq 0 && ! -z "$versionOutput" ]]; then
+    echo "Jenkins version: $versionOutput" >> ./output.log
+    # Continue with the next steps
+else
+    echo "ERROR: Jenkins version command returned empty or an error occurred. Exiting." >> ./output.log
+    exit 1
+fi
 
 # Then install git
-sudo apt install -y git;
+log_command sudo apt install -y git
 
 # Then install terraform
-wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg;
+log_command wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
 
-echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list;
+log_command echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
 
-sudo apt update -y && sudo apt install terraform -y;
+log_command sudo apt update -y && sudo apt install terraform -y
 
 # Then install kubectl
-
-
-# Download the latest release with the command:
-curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl";
+log_command curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 
 # Download the kubectl checksum file:
-curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256";
+log_command curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
 
 # Validate the kubectl binary against the checksum file:
-echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check
+log_command echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check
 
 # Install kubectl
-sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+log_command sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 
-# Test to ensure the version you installed is up-to-date:
-sudo kubectl version --client
+# Run the command to get kubectl version
+versionOutput=$(kubectl version --client --short 2>&1)
+
+# Check if the command was successful and the output is not empty
+if [[ $? -eq 0 && ! -z "$versionOutput" ]]; then
+    echo "kubectl version: $versionOutput" >> ./output.log
+    # Continue with the next steps
+else
+    echo "ERROR: kubectl version command returned empty or an error occurred. Exiting." >> ./output.log
+    exit 1
+fi
+
+# Install nodejs for truffle
+log_command curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh;
+
+log_command curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+
+log_command source ~/.bashrc
+
+log_command nvm install v18.20.4
+
+node -v
--- jenkins-server/main.tf
+++ jenkins-server/main.tf
@@ -48,10 +48,10 @@ module "vpc" {
 
   azs            = data.aws_availability_zones.azs.names
   public_subnets = var.public_subnets
-  
+
   map_public_ip_on_launch = true
-  enable_dns_hostnames = true
-  enable_vpn_gateway   = false
+  enable_dns_hostnames    = true
+  enable_vpn_gateway      = false
 
   tags = {
     Name        = "jenkins-vpc"
@@ -68,6 +68,7 @@ module "vpc" {
 # Create EC2 instance
 module "ec2_instance" {
   source = "terraform-aws-modules/ec2-instance/aws"
+  ami    = data.aws_ami.ubuntu.id
 
   name = "jenkins-server"
 
@@ -78,7 +79,7 @@ module "ec2_instance" {
   subnet_id                   = module.vpc.public_subnets[0]
   associate_public_ip_address = true
   user_data                   = file("jenkins-installation.sh")
-  availability_zone           = data.aws_availability_zones.azs.names[0]
+  availability_zone = data.aws_availability_zones.azs.names[0]
 
   tags = {
     Name        = "Jenkins-Server"
